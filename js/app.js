f45f0850455a273710e6d16c797cd696395bcf4f
Daniel Jaimes <danielDAJJ@outlook.com>
2024-02-29 16:17:42 -0500

    Se inicia la creación de la función para validar los puntajes de los jugadores, además se agregan comentaros en las respectivas lineas

--- js/app.js
+++ js/app.js
@@ -9,9 +9,10 @@ let cartasSeleccionadaRival
 let victoriasJugador = 0;
 let victoriasMaquina = 0;
 
+let rondas = 5;
 //************************************* Funciones
 
-alert("hola " + jugador + " a continuación te enfrentaras en un juego de cartas contra tu " + maquina + " el objetivo es ganar 3 de 5 rondas en las que deberas escoger entre un conjunto de tres numeros, una vez hayas seleccionado tu número se compara el resultado con el de la máquina, el número mayor ganará");
+alert("Hola " + jugador + " a continuación te enfrentarás en un juego de cartas contra tu " + maquina + ". El objetivo es ganar 3 de 5 rondas en las que deberás escoger entre un conjunto de tres números, una vez hayas seleccionado tu número este se comparará con la carta de tu " + maquina + ". El número mayor ganará");
 
 cartasParaJugador();
 cartasMaquina();
@@ -36,10 +37,14 @@ function cartasParaJugador() {
             cartaSeleccionadaJugador = cartaJugador = Number(prompt("Debes elegir una de estas cartas: " + carta1 + " " + carta2 + " " + carta3));
             return cartaJugador
         }}
-    };
+    }; // función que valida la elección del usuario y asigna a la variable global cartaSeleccionadaJugador la carta elegida por el jugador
 
 function cartasMaquina() {
     cartasSeleccionadaRival = generarCartas(1, 13);
     return alert("Tu " + maquina + " ha seleccionado " + cartasSeleccionadaRival)
-}
+}// función para generar la carta de la máquina y asignarla a la variable global cartasSeleccionadaRival
+
+function puntaje(params) {
+    
+}// función para para validar la puntuación de los usuarios
 
