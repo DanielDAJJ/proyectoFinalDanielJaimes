668234a34a45a7b1380f4caa4fa1629179a0ef97
Daniel Jaimes <danielDAJJ@outlook.com>
2024-03-01 21:47:34 -0500

    Se realiza la función para crear el bucle del juego, de realiza una opción con el bucle for, pero analizando se decide dejar esta con bucle while

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ let cartaSeleccionadaRival
 let victoriasJugador = 0;
 let victoriasMaquina = 0;
 
-let rondas = 0;
+let rondas = 1;
 //************************************* Funciones
 
 alert("Hola " + jugador + " a continuación te enfrentarás en un juego de cartas contra tu " + maquina + ". El objetivo es ganar 3 de 5 rondas en las que deberás escoger entre un conjunto de tres números, una vez hayas seleccionado tu número este se comparará con la carta de tu " + maquina + ". El número mayor ganará");
@@ -17,6 +17,7 @@ alert("Hola " + jugador + " a continuación te enfrentarás en un juego de carta
 cartasParaJugador();
 cartasMaquina();
 puntaje();
+sumarRondas();
 
 function generarCartas(min, max) {
     return Math.floor(Math.random()*(max - min + 1) +min);
@@ -47,16 +48,32 @@ function cartasMaquina() {
 
 function puntaje() {
     if (cartaSeleccionadaJugador < cartaSeleccionadaRival) {
-        victoriasMaquina =+1;
+        victoriasMaquina +=1;
         console.log("tu " + maquina  + " tiene el puntaje más alto. Haz perdido esta ronda");
         alert("Ronda " + rondas + " gana tu " + maquina);
     } else if (cartaSeleccionadaJugador > cartaSeleccionadaRival) {
-        victoriasJugador =+1;
+        victoriasJugador +=1;
         console.log(jugador + " tiene el puntaje más alto. Has ganado esta ronda!");
-        alert("Ronda " + rondas + jugador + " ha ganado");
+        alert("Ronda " + rondas + " " + jugador + " ha ganado");
     } else{
         console.log("Ambos jugadores tienen el mismo puntaje. Es un empate");
         alert("Ronda " + rondas + " ha sido un empate");
     }
+    console.log("puntaje jugador " + victoriasJugador + " puntaje rival " + victoriasMaquina);
     }// función para para comparar la puntuación de los usuarios
+
+function sumarRondas() {
+    while (rondas < 5 && victoriasJugador < 3 && victoriasMaquina < 3) {
+        rondas +=1;
+        cartasParaJugador();
+        cartasMaquina();
+        puntaje();
+    }
+    /* rondas += 1; 
+    for (rondas; rondas < 5; rondas ++) {
+        cartasParaJugador();
+        cartasMaquina();
+        puntaje();        
+    } este bucle funciona pero no evalua las victorias de los jugadores*/
+}//función para sumar las rondas y reiniciar el juego
 
