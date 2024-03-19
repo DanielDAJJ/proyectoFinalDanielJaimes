a92b8eb954bc6e5c8256f645561a8a88a4721428
Daniel Jaimes <danielDAJJ@outlook.com>
2024-03-19 13:50:34 -0500

    Se finaliza esta versión del pryecto preguntar sobro como resolver el bug de mayor generación de cartas extra que no se muestran pero quedan almacenadas en la consola

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@ let rondas = 1;
 
 alert("Hola " + jugador + " a continuación te enfrentarás en un juego de cartas contra tu " + maquina + ". El objetivo es ganar 3 de 5 rondas en las que deberás escoger entre un conjunto de tres números, una vez hayas seleccionado tu número este se comparará con la carta de tu " + maquina + ". El número mayor ganará");
 
+
 manoJugador();
 escogerCartaJugador();
 escogerCartaRival();
@@ -53,13 +54,13 @@ function generarPalo() {
     }
 }// función para devolver el palo de la carta
 function manoJugador() {
-    if (cartasJugador.length <= 3 && cartasRival.length <= 3) {
-        for (let i = 0; i <= 3; i+=1) {
+    if (cartasJugador.length <= 2 && cartasRival.length <= 2) {
+        for (let i = 0; i <= 2; i+=1) {
             const cartaJugador = new cartas (generarNumero(1,13), generarPalo());
             const cartaRival = new cartas (generarNumero(1,13), generarPalo());
             cartasJugador.push(cartaJugador);
             cartasRival.push(cartaRival);
-        }
+        } 
     }
     console.log(cartasJugador);
 }//Función para crear nuevas cartas en la mano de los jugadores
@@ -145,5 +146,3 @@ function victoria() {
         sumarRondas();
     }
 }
-
-//-------- Función para resolver bugs
