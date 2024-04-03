cbc00bbc9226bf4cd5a51eb1d6dafd7fc00ad6af
Daniel Jaimes <danielDAJJ@outlook.com>
2024-04-03 09:41:15 -0500

    Se establecen las variables que llaman a los elementos del DOM y se crean los arrays de la mano del jugador

--- js/app.js
+++ js/app.js
@@ -18,3 +18,24 @@ Lista de tareas
 ► Función de sumar ronda y repetir proceso
 ► Función de historial de jugadas
 */
+
+/******************************************Variables globales*****************************************/
+
+let btnNuevaPartida = document.querySelector("#BtnNuevaPartida");//Boton iniciar partida
+let nombre = document.querySelector("#POPNombre");//Alert para introducir el nombre del jugador
+let nombreForm = document.querySelector("#nombreForm");//Formulario para introducir el nombre
+let inputNombre = document.querySelector("#POPNick");//Input para poner el nombre
+let btnNombre = document.querySelector("#btnNombre");//Boton para enviar el nombre
+let instrucciones = document.querySelector("#instruccion");//
+let nombreJugador = document.querySelector("#NombreJ");
+let btnInstrucciones = document.querySelector("#instruccionBTN")
+let tablero = document.querySelector("#tablero");
+let pasarTurno = document.querySelector("#pasarTurno");
+let temporizador = document.querySelector("#temporizador");
+let ganador = document.querySelector("#ganador");
+
+const manoJugador = [];
+const manoRival = [];
+
+
+
